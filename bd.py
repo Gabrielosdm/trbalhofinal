62d157620fe4d8bed48128fa829ecbbffb42cdb3
gabrielpsdm <gabrielpsdm@hotmail.com>
2019-05-16 21:42:26 -0300

    login func

--- bd.py
+++ bd.py
@@ -1,9 +1,9 @@
 def validarlogin(cursor, nome, senha):
     # Executar o SQL
-    cursor.execute('select idfuncionarios from funcionarios WHERE nome = "{nome} " and senha = "{senha}"')
+    cursor.execute(f'select idfuncionarios from funcionarios WHERE nome = "{nome} " and senha = "{senha}"')
 
     # Recuperando o retorno do BD
-    funcionarios = cursor.fetchall()
+    funcionarios = cursor.fetchone()
 
     cursor.close()
 
